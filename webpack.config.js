8cf09cb41ba9c876ca2ea8c5b8f26bcae2f03fff
Kirill <inconsumable1812@gmail.com>
2020-11-11 21:01:03 +0800

    Подключение pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require('path')
+const fs = require('fs')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 //const { ALL } = require('dns')
 const CopyWepbackPlugin = require('copy-webpack-plugin')
 const MiniCssWebpackPlugin = require('mini-css-extract-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+//const TerserWebpackPlugin = require('terser-webpack-plugin')
 
-/*const PATHS = {
-    src: path.join(__dirname, '../src'),
-    dist: path.join(__dirname, '../dist'),
-    assets: 'assets/'
+const PATHS = {
+  // src must be src
+  src: path.join(__dirname, 'src'),
+  // dist to public
+  // dist: path.join(__dirname, '../public'),
+  // assets to static
+  //assets: 'static/'
 }
 
+const PAGES_DIR = `${PATHS.src}/pug/`
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith('.pug'))
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      //new TerserWebpackPlugin(),
+    ]
+  }
+
+  return config
+}
+
+/*
 const PAGES_DIR = `${PATHS.src}/pug/`
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))*/
 
@@ -31,15 +63,25 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },*/
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+  optimization: optimization(),
+
   plugins: [
-    new HTMLWebpackPlugin({
+    /* new HTMLWebpackPlugin({
       template: './index.html',
-    }),
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),*/
+    ...PAGES.map(
+      (page) =>
+        new HTMLWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, '.html')}`,
+          minify: {
+            collapseWhitespace: isProd,
+          },
+        })
+    ),
     new CleanWebpackPlugin(),
     new CopyWepbackPlugin({
       patterns: [
@@ -56,8 +98,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+      {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            /*options: {
+              hmr: true,
+              reloadAll: true,
+            },*/
+          },
+          'css-loader',
+        ],
       },
       {
         test: /\.(png|jpg|svg)$/,
