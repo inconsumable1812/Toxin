095afdd226385af333d754b77e6f4b7545b84190
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-08-07 16:53:24 +0800

    Начало написания JS для dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,1 +1,24 @@
+/*const buttonsMinus = document.querySelectorAll(
+  '.dropdown__counter-buttons_type_minus'
+)
+const buttonsPlus = document.querySelectorAll(
+  '.dropdown__counter-buttons_type_plus'
+)
 
+for (const buttonMinus of buttonsMinus) {
+  buttonMinus.addEventListener('click', () => console.log('минус'))
+}*/
+
+const counterButtons = document.querySelectorAll('.dropdown__counter-buttons')
+for (const counterButton of counterButtons) {
+  const buttonsMinus = counterButton.querySelector(
+    '.dropdown__counter-buttons_type_minus'
+  )
+  const buttonsPlus = counterButton.querySelector(
+    '.dropdown__counter-buttons_type_plus'
+  )
+  const counter = counterButton.querySelector('.dropdown__counter')
+  console.log(counter)
+}
+
+function solutionCount() {}
--- src/pages/FormElements/FormElements.js
+++ src/pages/FormElements/FormElements.js
@@ -1,3 +1,4 @@
 import $ from 'jquery'
 import './FormElements.scss'
 import '../../components/input/input'
+import '../../components/dropdown/dropdown'
