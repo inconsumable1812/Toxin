cde19d5fab37a7e7d08f7a5a91b113a85498b88c
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-08-11 16:53:54 +0800

    Доработка логики счетчика dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,43 +1,9 @@
-const counterButtons = document.querySelectorAll('.dropdown__counter-buttons')
+//const counterButtons = document.querySelectorAll('.dropdown__counter-buttons')
 const dropdownButtons = document.querySelectorAll('.dropdown__button')
 const dropdownBoxes = document.querySelectorAll('.dropdown__box')
 const nameBedrooms = ['спальня', 'спальни', 'спален']
 const nameBeds = ['кровать', 'кровати', 'кроватей']
 const nameBathrooms = ['ванная комната', 'ванных комнаты', 'ванных комнат']
-
-for (const counterButton of counterButtons) {
-  const buttonsMinus = counterButton.querySelector(
-    '.dropdown__counter-buttons_type_minus'
-  )
-  const buttonsPlus = counterButton.querySelector(
-    '.dropdown__counter-buttons_type_plus'
-  )
-  const counter = counterButton.querySelector('.dropdown__counter')
-  let counterInt = parseInt(counter.innerHTML)
-
-  buttonsMinus.addEventListener('click', () => {
-    if (counterInt > 0) {
-      counterInt = --counterInt
-    }
-    counter.innerHTML = `${counterInt}`
-    if (counterInt === 0) {
-      buttonsMinus.classList.add(
-        'dropdown__counter-buttons_type_minus_disabled'
-      )
-    }
-  })
-  buttonsPlus.addEventListener('click', () => {
-    if (counterInt < 20) {
-      counterInt = ++counterInt
-    }
-    counter.innerHTML = `${counterInt}`
-    if (counterInt > 0) {
-      buttonsMinus.classList.remove(
-        'dropdown__counter-buttons_type_minus_disabled'
-      )
-    }
-  })
-}
 
 for (const dropdownBox of dropdownBoxes) {
   const dropdownButton = dropdownBox.querySelector('.dropdown__button')
@@ -45,6 +11,7 @@ for (const dropdownBox of dropdownBoxes) {
   const dropdownInput = dropdownBox.querySelector('.dropdown__input')
   const clearButton = dropdownBox.querySelector('.dropdown__clear-buttons')
   const applyButton = dropdownBox.querySelector('.dropdown__apply-buttons')
+  const counterButtons = dropdownBox.querySelectorAll('.dropdown__counter-buttons')
 
   dropdownButton.addEventListener('click', () => {
     if (dropdownList.classList.contains('dropdown__list_expanded')) {
@@ -56,11 +23,52 @@ for (const dropdownBox of dropdownBoxes) {
     }
   })
 
-  // appleRooms(applyButton)
-}
+  for (const counterButton of counterButtons) {
+    const buttonsMinus = counterButton.querySelector('.dropdown__counter-buttons_type_minus')
+    const buttonsPlus = counterButton.querySelector('.dropdown__counter-buttons_type_plus')
+    const counter = counterButton.querySelector('.dropdown__counter')
+    let counterInt = parseInt(counter.innerHTML)
+    const countName = counterButton.parentElement.querySelector('.dropdown__item-name').innerHTML
+    console.log(countName)
+    console.log(counterInt)
+    let numberOfBedrooms = counterInt
+    let numberOfBeds = counterInt
+    let numberOfBathrooms = counterInt
 
-/*function appleRooms(button) {
-  button.addEventListener('click', (event) => {
-    console.log('button')
-  })
-}*/
+    buttonsMinus.addEventListener('click', () => {
+      if (counterInt > 0) {
+        counterInt = --counterInt
+      }
+      counter.innerHTML = `${counterInt}`
+      if (counterInt === 0) {
+        buttonsMinus.classList.add('dropdown__counter-buttons_type_minus_disabled')
+      }
+      if (counter.classList.contains('dropdown__counter_rooms')) {
+      }
+    })
+    buttonsPlus.addEventListener('click', () => {
+      if (counterInt < 20) {
+        counterInt = ++counterInt
+      }
+      counter.innerHTML = `${counterInt}`
+      if (counterInt > 0) {
+        buttonsMinus.classList.remove('dropdown__counter-buttons_type_minus_disabled')
+      }
+      switch (countName) {
+        case 'спальни':
+          numberOfBedrooms = counterInt
+          break
+        case 'кровати': {
+          numberOfBeds = counterInt
+          break
+        }
+        case 'ванные комнаты': {
+          numberOfBathrooms = counterInt
+          break
+        }
+      }
+
+      dropdownInput.placeholder = `${numberOfBedrooms} ${numberOfBeds} ${numberOfBathrooms}`
+    })
+  }
+}
