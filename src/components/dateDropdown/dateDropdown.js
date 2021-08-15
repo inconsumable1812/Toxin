dae589b1dded0cf98ebb7a69d50df9ad89070d91
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-08-15 21:32:14 +0800

    Стили для календаря

--- src/components/dateDropdown/dateDropdown.js
+++ src/components/dateDropdown/dateDropdown.js
@@ -5,8 +5,6 @@ const dateDropdownItems = document.querySelectorAll('.dateDropdown__item')
 const dateDropdownsDouble = document.querySelectorAll('.dateDropdown__group_double')
 //const inputFrom = document.querySelectorAll('input[name="field-date-from"]')
 //const inputTo = document.querySelectorAll('input[name="field-date-to"]')
-const applyButton = document.createElement('div')
-applyButton.classList.add('dropdown__apply-buttons')
 
 for (const dateDropdownItem of dateDropdownItems) {
   const dropdownBox = dateDropdownItem.querySelector('.dateDropdown__dropdownBox')
@@ -49,11 +47,6 @@ $(function () {
     },
   })
 
-  let $isPickerButtons = $('.date-picker').find('.datepicker--buttons')
-  if ($isPickerButtons) {
-    $isPickerButtons.append(applyButton)
-  }
-
   addApplyButton()
 
   let $applyButton = $('[data-action="apply"]')
@@ -70,37 +63,4 @@ $(function () {
     let $isDatePicker = $('.dropdown__calendar_content')
     $isDatePicker.removeClass('js-calendar_content_expanded')
   }
-
-  /*$('.dateDropdown_double .js-calendar_content').each(function() {
-    console.log($(this))
-    $(this).datepicker({
-      multipleDates: 2,
-      minDate: new Date(),
-      range: 'true',
-      multipleDatesSeparator: ' - ',
-      clearButton: 'true',
-      onSelect: function (fd, d, picker) {
-        $('input[name="field-date-from"]').val(fd.split('-')[0])
-        $('input[name="field-date-to"]').val(fd.split('-')[1])
-      },
-    })
-  })*/
 })
-
-/*$(function () {
-    addApplyButton()
-
-    let $applyButton = $('[data-action="apply"]')
-    $applyButton.on('click', hideDatePicker)
-  })
-  function addApplyButton() {
-    let $isPickerButtons = $('.date-picker').find('.datepicker--buttons')
-    if ($isPickerButtons) {
-      $isPickerButtons.append('<span class="datepicker--button" data-action="apply">Применить</span>')
-    }
-  }
-
-  function hideDatePicker() {
-    let $isDatePicker = $('.dropdown-date__date-picker')
-    $isDatePicker.css('display', 'none')
-  }*/
