4f564ae81e3870399c5c3fc39d79e29348833a23
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-08-30 22:06:58 +0800

    Сделал responsive

--- src/pages/searchRoom/searchRoom.js
+++ src/pages/searchRoom/searchRoom.js
@@ -6,3 +6,19 @@ import '../../components/rangeSlider/rangeSlider'
 import '../../components/pagination/pagination'
 import '../../components/imageSlider/imageSlider'
 import '../../components/header/header'
+
+const openButton = document.querySelector('.searchRoom__button-open').querySelector('.button')
+const closeButton = document.querySelector('.searchRoom__button-close')
+const filter = document.querySelector('.searchRoom__filter')
+
+openButton.addEventListener('click', () => {
+  if (!filter.classList.contains('searchRoom__filter_expanded')) {
+    filter.classList.add('searchRoom__filter_expanded')
+  } else {
+    filter.classList.remove('searchRoom__filter_expanded')
+  }
+})
+
+closeButton.addEventListener('click', () => {
+  filter.classList.remove('searchRoom__filter_expanded')
+})
