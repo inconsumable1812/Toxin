fbe03017040f756fd53c39fdea401a6c6d04abea
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-09-01 20:49:23 +0800

    Сделал страницы registration и sign-in

---
+++ src/pages/registration/registration.js
@@ -0,0 +1,5 @@
+import './registration.scss'
+
+import '../../components/header/header'
+import '../../components/imageSlider/imageSlider'
+import '../../components/registrationForm/registrationForm'
---
+++ src/pages/sign-in-page/sign-in-page.js
@@ -0,0 +1,3 @@
+import './sign-in-page.scss'
+import '../../components/header/header'
+import '../../components/imageSlider/imageSlider'
