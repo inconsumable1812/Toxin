df96e57e925699e3ae07333c1115d62782f045ca
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-11-23 14:24:09 +0800

    fix rename

--- src/components/chart/Chart-init.js
+++ src/components/chart/Chart-init.js
@@ -1,4 +1,4 @@
-import ChartElement from './_Chart';
+import ChartElement from './Chart';
 
 const charts = document.querySelectorAll('.js-chart__item');
 charts.forEach((chart) => new ChartElement(chart));
